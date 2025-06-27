ef44b67c323547bc7674c26c007409b56311c34e
Muaz Hussain <muaz.self@gmail.com>
2025-06-27 17:03:50 +0600

    Database configuration setup

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int) -> int:
+    """Simple add function"""
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Simple subtract function"""
+    return x - y
